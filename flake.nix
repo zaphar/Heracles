8dc243b9a1c9f94fc23564d80267d58a6c79b158
Jeremy Wall <jeremy@marzhillstudios.com>
2024-02-19 16:56:14 -0500

    maint: linux nix builds?

--- flake.nix
+++ flake.nix
@@ -21,9 +21,7 @@
           rustc = rust-bin;
           cargo = rust-bin;
       };
-    in
-    {
-      packages.default = naersk-lib.buildPackage {
+      heracles = naersk-lib.buildPackage {
           name = "heracles";
           verion = "0.0.1";
           src = ./.;
@@ -31,7 +29,125 @@
             if pkgs.stdenv.isDarwin then
               with pkgs.darwin.apple_sdk.frameworks; [ Security SystemConfiguration ]
             else
-            [ ]) ++ [rust-bin];
+            [ pkgs.openssl ]) ++ [rust-bin];
       };
-    });
+    in
+    {
+      packages.default = heracles;
+    }) // {
+        nixosModule = {config, pkgs, lib}: with lib; {
+            options = {
+                services.heracles.enable = mkEnableOption "enable heracles service";
+                services.heracles.listen = mkOption {
+                    description = "[host]:port address for heracles to listen on";
+                    default = "localhost:8080";
+                    defaultText = "localhost:8080";
+                };
+
+                services.heracles.settings = mkOption {
+                    description = "heracles dashboard Configuration";
+                    default = [
+                        {
+                            title = "A dashboard";
+                            graphs = [
+                                {
+                                    title = "Graph title";
+                                    query_type = "Range";
+                                    # yaxis formatting default for this graph
+                                    d3_tick_format = "~s";
+                                    plots = [
+                                        {
+                                            source = "http://heimdall:9001";
+                                            query = ''
+                                                sum by (instance)(irate(node_cpu_seconds_total{job="nodestats"}[5m]))
+                                            '';
+                                            meta = {
+                                                name_label = "instance";
+                                                name_prefix = "trace name prefix";
+                                                name_suffix = "trace name suffix";
+                                                named_axis = "y";
+                                                # yaxis formatting for this subplot
+                                                d3_tick_format = "~s";
+                                            };
+                                        }
+                                    ];
+                                    # span for this graph.
+                                    span = {
+                                        end = "now";
+                                        duration = "1d";
+                                        step_duration = "10min";
+                                    };
+                                }
+                            ];
+                            # default span for dashboard
+                            span = {
+                                end = "now";
+                                duration = "1d";
+                                step_duration = "10min";
+                            };
+                        }
+                    ];
+                    defaultText = ''
+                    [
+                        {
+                            title = "A dashboard";
+                            graphs = [
+                                {
+                                    title = "Graph title";
+                                    query_type = "Range";
+                                    # yaxis formatting default for this graph
+                                    d3_tick_format = "~s";
+                                    plots = [
+                                        {
+                                            source = "http://heimdall:9001";
+                                            query = \'\'
+                                                sum by (instance)(irate(node_cpu_seconds_total{job="nodestats"}[5m]))
+                                            \'\';
+                                            meta = {
+                                                name_label = "instance";
+                                                name_prefix = "trace name prefix";
+                                                name_suffix = "trace name suffix";
+                                                named_axis = "y";
+                                                # yaxis formatting for this subplot
+                                                d3_tick_format = "~s";
+                                            };
+                                        }
+                                    ];
+                                    # span for this graph.
+                                    span = {
+                                        end = "now";
+                                        duration = "1d";
+                                        step_duration = "10min";
+                                    };
+                                }
+                            ];
+                            # default span for dashboard
+                            span = {
+                                end = "now";
+                                duration = "1d";
+                                step_duration = "10min";
+                            };
+                        }
+                    ]
+                    '';
+                };
+            };
+
+            config = mkIf config.services.heracles.enable {
+                environment.etc."heracles.yaml" = {
+                    text = (generators.toYAML {} config.services.heracles.settings);
+                };
+                systemd.services.heracles = {
+                    wantedBy = [ "multi-user.target" "default.target" ];
+                    wants = [ "network.target" ];
+                    after = [ "network-online.target" ];
+                    serviceConfig = {
+                        Restart = "on-failure";
+                        RestartSec = "30s";
+                        ExecStart = "${pkgs.heracles}/bin/heracles --listen ${config.services.heracles.listen} --config=${config.environment.etc."heracles.yaml".target}";
+                    };
+                };
+            };
+        };
+    };
 }
